45d9a704b4c7db18256f688233d0a817909f48ec
kjjgibson <kennethjjgibson@gmail.com>
2016-09-20 18:20:52 +1000

    Remove FastAdapter for scroll items list

--- app/src/main/java/com/giraffetech/decisivewizard/fragment/CreateScrollFragment.java
+++ app/src/main/java/com/giraffetech/decisivewizard/fragment/CreateScrollFragment.java
@@ -5,28 +5,22 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.giraffetech.decisivewizard.R;
+import com.giraffetech.decisivewizard.adapter.ScrollItemAdapter;
 import com.giraffetech.decisivewizard.listener.ScrollItemListItemHandler;
 import com.giraffetech.decisivewizard.listitem.AddScrollItemListItem;
 import com.giraffetech.decisivewizard.listitem.ScrollItemListItem;
-import com.mikepenz.fastadapter.FastAdapter;
-import com.mikepenz.fastadapter.IAdapter;
-import com.mikepenz.fastadapter.IItem;
-import com.mikepenz.fastadapter.adapters.FooterAdapter;
-import com.mikepenz.fastadapter.adapters.ItemAdapter;
-import com.mikepenz.fastadapter_extensions.drag.ItemTouchCallback;
-import com.mikepenz.fastadapter_extensions.drag.SimpleDragCallback;
 
 import java.util.ArrayList;
 
-public class CreateScrollFragment extends Fragment implements ItemTouchCallback, ScrollItemListItemHandler, FastAdapter.OnClickListener<IItem> {
+public class CreateScrollFragment extends Fragment implements ScrollItemListItemHandler {
 
-    private ItemAdapter<ScrollItemListItem> mScrollItemListItemItemAdapter;
+    private ScrollItemAdapter mScrollItemListItemItemAdapter;
+    private RecyclerView mRecyclerView;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -44,72 +38,75 @@ public class CreateScrollFragment extends Fragment implements ItemTouchCallback,
         View view = inflater.inflate(R.layout.fragment_create_scroll, container, false);
 
         Context context = view.getContext();
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewScrollListItems);
-        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewScrollListItems);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
 
         //Allow dragging and reordering items
-        SimpleDragCallback dragCallback = new SimpleDragCallback(this);
-        ItemTouchHelper touchHelper = new ItemTouchHelper(dragCallback);
-        touchHelper.attachToRecyclerView(recyclerView);
+//        SimpleDragCallback dragCallback = new SimpleDragCallback(this);
+//        ItemTouchHelper touchHelper = new ItemTouchHelper(dragCallback);
+//        touchHelper.attachToRecyclerView(mRecyclerView);
 
         //Set up the adapters
-        FooterAdapter<AddScrollItemListItem> mFooterAdapter = new FooterAdapter<>();
-        mScrollItemListItemItemAdapter = new ItemAdapter<>();
-        FastAdapter<IItem> mFastAdapter = new FastAdapter<>();
-        mFastAdapter.withSelectable(true);
-        mFastAdapter.withOnClickListener(this);
+//        FooterAdapter<AddScrollItemListItem> mFooterAdapter = new FooterAdapter<>();
+        mScrollItemListItemItemAdapter = new ScrollItemAdapter(getScrollItemListItems());
+//        FastAdapter<IItem> mFastAdapter = new FastAdapter<>();
+//        mFastAdapter.withSelectable(true);
+//        mFastAdapter.withOnClickListener(this);
 
         //Set the adapters on the RecyclerView
-        recyclerView.setAdapter(mFooterAdapter.wrap(mScrollItemListItemItemAdapter.wrap(mFastAdapter)));
+        mRecyclerView.setAdapter(mScrollItemListItemItemAdapter);
 
         //Add the items to the adapters
-        mScrollItemListItemItemAdapter.add(getScrollItemListItems());
-        mFooterAdapter.add(getFooterItems());
+//        mScrollItemListItemItemAdapter.add(getScrollItemListItems());
+//        mFooterAdapter.add(getFooterItems());
 
         return view;
     }
 
-    @Override
-    public boolean itemTouchOnMove(int oldPosition, int newPosition) {
-        boolean allowMove = false;
-        //Only allow the item to be moved if it is moving to a position in the ScrollItemListItem adapter
-        //Don't allow moving to the FooterAdapter
-        if (newPosition < mScrollItemListItemItemAdapter.getAdapterItemCount()) {
-            mScrollItemListItemItemAdapter.move(oldPosition, newPosition);
-            allowMove = true;
-        }
-        return allowMove;
-    }
+//    @Override
+//    public boolean itemTouchOnMove(int oldPosition, int newPosition) {
+//        boolean allowMove = false;
+//        //Only allow the item to be moved if it is moving to a position in the ScrollItemListItem adapter
+//        //Don't allow moving to the FooterAdapter
+//        if (newPosition < mScrollItemListItemItemAdapter.getItemCount()) {
+//            mScrollItemListItemItemAdapter.move(oldPosition, newPosition);
+//            allowMove = true;
+//        }
+//        return allowMove;
+//    }
 
     @Override
     public void onRemoveItemClick(ScrollItemListItem itemToRemove) {
-        mScrollItemListItemItemAdapter.remove(mScrollItemListItemItemAdapter.getAdapterItems().indexOf(itemToRemove));
+        int index = mScrollItemListItemItemAdapter.getItems().indexOf(itemToRemove);
+        mScrollItemListItemItemAdapter.getItems().remove(index);
+        mScrollItemListItemItemAdapter.notifyItemRemoved(index);
     }
 
     @Override
     public void addNewItemAfter(ScrollItemListItem item) {
-        addNewEmptyItem(mScrollItemListItemItemAdapter.getAdapterItems().indexOf(item) + 1);
+        addNewEmptyItem(mScrollItemListItemItemAdapter.getItems().indexOf(item) + 1);
     }
 
-    @Override
-    public boolean onClick(View v, IAdapter adapter, IItem item, int position) {
-        boolean eventConsumed = false;
-        if (adapter instanceof FooterAdapter) {
-            addNewEmptyItem(mScrollItemListItemItemAdapter.getItemCount() - 1);
-            eventConsumed = true;
-        }
-        return eventConsumed;
-    }
+//    @Override
+//    public boolean onClick(View v, IAdapter adapter, IItem item, int position) {
+//        boolean eventConsumed = false;
+//        if (adapter instanceof FooterAdapter) {
+//            addNewEmptyItem(mScrollItemListItemItemAdapter.getItemCount() - 1);
+//            eventConsumed = true;
+//        }
+//        return eventConsumed;
+//    }
 
     private void addNewEmptyItem(int position) {
         ScrollItemListItem item = new ScrollItemListItem("", this);
-        mScrollItemListItemItemAdapter.add(position, item);
+        mScrollItemListItemItemAdapter.getItems().add(position, item);
+        mScrollItemListItemItemAdapter.notifyItemInserted(position);
         item.setShouldHaveFocus(true);
     }
 
     private ArrayList<ScrollItemListItem> getScrollItemListItems() {
         final ArrayList<ScrollItemListItem> scrollItemListItems = new ArrayList<>();
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i <= 2; i++) {
             scrollItemListItems.add(new ScrollItemListItem("Item " + i, this));
         }
         return scrollItemListItems;
--- app/src/main/java/com/giraffetech/decisivewizard/listitem/ScrollItemListItem.java
+++ app/src/main/java/com/giraffetech/decisivewizard/listitem/ScrollItemListItem.java
@@ -1,25 +1,10 @@
 package com.giraffetech.decisivewizard.listitem;
 
-import android.databinding.DataBindingUtil;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
-import com.giraffetech.decisivewizard.R;
-import com.giraffetech.decisivewizard.databinding.ScrollItemListItemBinding;
 import com.giraffetech.decisivewizard.listener.ScrollItemListItemHandler;
-import com.mikepenz.fastadapter.items.AbstractItem;
 
-import java.util.List;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class ScrollItemListItem extends AbstractItem<ScrollItemListItem, ScrollItemListItem.ViewHolder> {
+public class ScrollItemListItem {
 
     //region Fields
     //Handler used to communicate back to the class containing the RecyclerView
@@ -48,6 +33,18 @@ public class ScrollItemListItem extends AbstractItem<ScrollItemListItem, ScrollI
     public String getItemText() {
         return mItemText;
     }
+
+    public boolean shouldHaveFocus() {
+        return mShouldHaveFocus;
+    }
+
+    public void shouldHaveFocus(boolean shouldHaveFocus) {
+        mShouldHaveFocus = shouldHaveFocus;
+    }
+
+    public ScrollItemListItemHandler getHandler() {
+        return mHandler;
+    }
     //endregion Getters and Setters
 
     //region Public Methods
@@ -55,94 +52,5 @@ public class ScrollItemListItem extends AbstractItem<ScrollItemListItem, ScrollI
         mShouldHaveFocus = shouldHaveFocus;
     }
     //endregion Public Methods
-
-    //region Fast Adapter Methods
-    @Override
-    public int getType() {
-        return R.id.scroll_item_list_item;
-    }
-
-    @Override
-    public int getLayoutRes() {
-        return R.layout.scroll_item_list_item;
-    }
-
-    @Override
-    public void bindView(final ViewHolder viewHolder, List payloads) {
-        super.bindView(viewHolder, payloads);
-
-        //Items should have focus when they are first added to the adapter
-        if (mShouldHaveFocus) {
-            viewHolder.getScrollItemEditText().requestFocus();
-            mShouldHaveFocus = false;
-        }
-
-        viewHolder.getScrollItemEditText().setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    viewHolder.getRemoveImageButton().setVisibility(View.VISIBLE);
-                } else {
-                    viewHolder.getRemoveImageButton().setVisibility(View.INVISIBLE);
-                }
-            }
-        });
-
-        viewHolder.getScrollItemEditText().addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                String string = s.toString();
-                //If the last character entered was a new line, add a new list item to the adapter
-                if (string.length() > 0 && string.substring(string.length() - 1).equals("\n")) {
-                    mHandler.addNewItemAfter(ScrollItemListItem.this);
-                    //Remove the new line character
-                    s.delete(string.length() - 1, string.length());
-                }
-            }
-        });
-
-        viewHolder.getBinding().setHandler(mHandler);
-        viewHolder.getBinding().setScrollItemListItem(this);
-        viewHolder.getBinding().executePendingBindings();
-    }
-
-    protected static class ViewHolder extends RecyclerView.ViewHolder {
-        private ScrollItemListItemBinding mBinding;
-
-        @BindView(R.id.editTextScrollItem)
-        EditText mScrollItemEditText;
-
-        @BindView(R.id.imageButtonRemoveItem)
-        ImageButton mRemoveImageButton;
-
-        public ViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-            mBinding = DataBindingUtil.bind(view);
-        }
-
-        protected ScrollItemListItemBinding getBinding() {
-            return mBinding;
-        }
-
-        protected EditText getScrollItemEditText() {
-            return mScrollItemEditText;
-        }
-
-        protected ImageButton getRemoveImageButton() {
-            return mRemoveImageButton;
-        }
-    }
-    //endregion Fast Adapter Methods
 
 }
